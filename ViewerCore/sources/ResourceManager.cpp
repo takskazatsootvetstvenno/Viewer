35e15d3963d7cac1e96492860b44e489725b70f4
Titaev Denis <dtitaev@vrconcept.net>
2021-11-09 14:07:55 +0300

    Add unix support

--- ViewerCore/sources/ResourceManager.cpp
+++ ViewerCore/sources/ResourceManager.cpp
@@ -43,7 +43,7 @@ namespace TestEngine {
 	{
 		addShaderProgram("General", "Shaders/General.vert", "Shaders/General.frag");
 		addShaderProgram("Red", "Shaders/Red.vert", "Shaders/Red.frag");
-		addModel("Models/map.objFile");
+		addModel("Models/map.obj");
 
 		BufferLayout buffer_layout_1mat4
 		{
